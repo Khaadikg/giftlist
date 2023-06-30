8a771196ea48e3b11809e529fb7c59f8be9135c9
user <emilbek.ergeshali@gmail.com>
2023-06-30 20:49:52 +0300

    examination branch

--- src/main/java/com/peaksoft/giftlistm5/controller/AuthController.java
+++ src/main/java/com/peaksoft/giftlistm5/controller/AuthController.java
@@ -1,26 +1,40 @@
-package com.peaksoft.giftlistm5.controller;
+package com.peaksoft.giftlistm5.Controller;
 
-import com.peaksoft.giftlistm5.dto.UserRequest;
-import com.peaksoft.giftlistm5.dto.UserResponse;
+import com.peaksoft.giftlistm5.dto.*;
 import com.peaksoft.giftlistm5.model.User;
+import com.peaksoft.giftlistm5.repository.UserRepository;
+import com.peaksoft.giftlistm5.security.jwt.JwtTokenFilter;
+import com.peaksoft.giftlistm5.security.jwt.JwtTokenUtil;
 import com.peaksoft.giftlistm5.service.UserService;
 import lombok.RequiredArgsConstructor;
-import org.slf4j.LoggerFactory;
-import org.springframework.ui.Model;
-import org.springframework.validation.BindingResult;
-import org.springframework.web.bind.annotation.*;
-
-import javax.validation.Valid;
-import java.util.logging.Logger;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("/api/auth")
+@RequestMapping("/api/jwt")
 public class AuthController {
     private final UserService userService;
+    private final JwtTokenUtil jwtTokenUtil;
+    private final UserRepository userRepository;
+    private final LoginMapper loginMapper;
+    private final AuthenticationManager authenticationManager;
 
-    @PostMapping("sign-up")
-    public UserResponse signUp(@RequestBody UserRequest request) throws Exception {
-        return userService.registration(request);
+    @PostMapping("sing-up")
+    public UserResponse signIn(@RequestBody UserRequest userRequest){
+        return userService.create(userRequest);
+    }
+
+    @PostMapping("sign-in")
+    public LoginResponse signIn(@RequestBody LoginRequest loginRequest){
+        UsernamePasswordAuthenticationToken token =
+                new UsernamePasswordAuthenticationToken(loginRequest.getEmail(),loginRequest.getPassword());
+        authenticationManager.authenticate(token);
+        User user = userRepository.findByEmail(token.getName()).get();
+        return loginMapper.loginView(jwtTokenUtil.generateToken(user),"Successful",user );
     }
 }
